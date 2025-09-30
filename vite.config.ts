f358c92bbb03fbbaf21dbfb2dec80370bc645379
jstzwj <1103870790@qq.com>
2025-09-30 23:44:37 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -30,11 +30,13 @@ export default defineConfig({
         vite: {
           build: {
             outDir: 'dist-electron',
+            lib: {
+              entry: 'electron/preload.ts',
+              formats: ['cjs'],
+              fileName: () => 'preload.js'
+            },
             rollupOptions: {
-              external: ['electron'],
-              output: {
-                format: 'cjs'
-              }
+              external: ['electron', 'custom-electron-titlebar']
             }
           }
         }

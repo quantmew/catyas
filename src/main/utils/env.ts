b5786f04bb14ed66ce8116591f8545e0334b1534
jstzwj <1103870790@qq.com>
2025-09-30 20:18:17 +0800

    clean code

--- src/main/utils/env.ts
+++
@@ -1,9 +0,0 @@
-export function isDev(): boolean {
-  return process.env.NODE_ENV === 'development' || !app.isPackaged
-}
-
-export function isProd(): boolean {
-  return !isDev()
-}
-
-import { app } from 'electron'
